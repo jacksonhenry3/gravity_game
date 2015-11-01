53526625b483d0da0edfd34af91955cf3059ab51
jacksonhenry3 <jackson.henry3@gmail.com>
2015-11-01 15:34:37 -0500

    mouse coord detection and relative coordinate system conversion

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+w = $( window ).width();
+h = $( window ).height();
 
+absolute_mouse_position = new vector([0,0])
+center                  = new vector([w/2,h/2])
+
+
+function convert_abs_to_rel(abs_vec){
+  rel_vec = new vector([abs_vec.x-center.x,-abs_vec.y+center.y]);
+  return(rel_vec)
+}
+
+function convert_rel_to_abs(rel_vec){
+  abs_vec = new vector([rel_vec.x+center.x,-rel_vec.y+center.y]);
+  return(abs_vec)
+}
+
+
+$( "html" ).mousemove(function( event ) {
+  absolute_mouse_position.x = event.pageX
+  absolute_mouse_position.y = event.pageY
+  relative_mouse_position = convert_abs_to_rel(absolute_mouse_position)
+
+  $("#mAbsCoords").html("The absolute mouse cooridnates are "+ absolute_mouse_position.x+","+absolute_mouse_position.y)
+  $("#mRelCoords").html("The relative mouse cooridnates are "+ relative_mouse_position.x+","+relative_mouse_position.y)
+});
---
+++ js/vector.js
@@ -0,0 +1,124 @@
+function vector (vals) {
+	this.length = 2;
+	this.x      = vals[0];
+	this.y      = vals[1];
+	this.vals   = vals
+
+	if (vals.length === 3)
+	{
+		this.length = 3;
+		this.z = vals[2];
+	}
+
+	this.subtract = function(v)
+	{
+		if (this.length!=v.length)
+		{
+			throw "Vectors must be the same size";
+		}
+		dx = this.x-v.x;
+		dy = this.y-v.y;
+		answer = new vector([dx,dy])
+		if (this.length == 3)
+		{
+			dz = this.z-v.z;
+			answer = new vector([dx,dy,dz])
+		}
+		return(answer)
+	};
+
+	this.add = function(v)
+	{
+		if (this.length!=v.length)
+		{
+			throw "Vectors must be the same size";
+		}
+		dx = this.x+v.x;
+		dy = this.y+v.y;
+		answer = new vector([dx,dy])
+		if (this.length == 3)
+		{
+			dz = this.z+v.z;
+			answer = new vector([dx,dy,dz])
+		}
+		return(answer)
+	};
+
+	this.scalarAdd = function(n)
+	{
+		dx = this.x+n
+		dy = this.y+n
+		answer = new vector([dx,dy])
+		if (this.length === 3)
+		{
+			dz = this.z+n
+			answer = new vector([dx,dy,dz])
+		}
+
+		return(answer);
+	}
+
+	this.dot = function(v)
+	{
+		if (this.length!=v.length)
+		{
+			throw "Vectors must be the same size";
+		}
+		var total = this.x*v.x+this.y*v.y;
+		if (this.length ===3)
+		{
+			total+=this.z*v.z;
+		}
+		return(total);
+	};
+
+	this.scale = function(scale)
+	{
+		dx = this.x*scale;
+		dy = this.y*scale;
+		answer = new vector([dx,dy])
+		if (this.length===3)
+		{
+			dz = this.z*scale;
+			answer = new vector([dx,dy,dz])
+		}
+		return(answer);
+	};
+
+	this.magnitude = function()
+	{
+		return(Math.sqrt(this.dot(this)));
+	};
+
+	this.norm = function()
+	{
+		return(this.scale(1/this.magnitude()));
+	};
+}
+
+function randomVector(dim)
+{
+	if (dim != (2 || 3))
+		{
+			throw "Vectors must be 2 or 3 dimensions";
+		};
+	var a  = Math.random()*2-1,
+	    b  = Math.random()*2-1,
+		rv = new vector([a,b]);
+	if (dim == 3)
+	{
+		var c  = Math.random()*2-1,
+			rv = new vector([a,b,c]);
+	}
+	return(rv)
+};
+
+function zeroVector(dim)
+{
+	var v = new vector([0,0]);
+	if (dim === 3)
+	{
+		v = new vector([0,0,0])
+	};
+	return(v)
+}
