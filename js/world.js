fd6eb26142ce905689c1fa8df7e35c2e4e70c628
Nico <dominic_adams@brown.edu>
2015-11-28 19:05:18 -0500

    Removed an extraneous console log

--- js/world.js
+++ js/world.js
@@ -112,8 +112,6 @@ function resolveCollision(player1, planet1) {
   // Changes the velocity (if the player is traveling into the planet)
   velDirection = player1.vel.dot(distVector)
   if(velDirection < 0) {
-    console.log("Reversing the velocity")
-    
     // Calculates the parallel component of the player's velocity and reverses it
     parallelVel = player1.vel.project(distVector)
     player1.vel = player1.vel.subtract(parallelVel.scale(2))
